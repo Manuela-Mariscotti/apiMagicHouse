3cba5c4f8c109fcc4a87de93608d42b883603fe8
DanielVazquez <danielvazquezheredia1994@gmail.com>
2022-09-18 20:57:51 +0200

    login controller

--- app.js
+++ app.js
@@ -22,10 +22,9 @@ app.use( (req, res, next) => {
     res.status(404).json({
         error:true,
         code:404,
-        message: 'RESOURCE NOT FOUND --> ' + err.message
+        message: 'RESOURCE NOT FOUND'
     })
 
-    next();
 })
 app.use(errorHandling);
 
