a5a8acab78524787936a093c19dc9bcae7ba816b
Daniel <danielvazquezheredia1994@gmail.com>
2022-09-22 18:00:05 +0200

    transactions !!

--- router/spents.router.js
+++ router/spents.router.js
@@ -9,4 +9,4 @@ router.get('/divide', spentsCtrl.divide)
 router.get('/isTransactionUpdated', spentsCtrl.isTransactionUpdated)
 router.get('/transactions', spentsCtrl.getTransactionsFromDB)
 
-module.exports = router;
+module.exports = router; 
