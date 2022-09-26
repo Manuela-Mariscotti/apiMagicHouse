e8ed85da7cd2b3c4131ee929d61a3a0bbe5c954e
DanielVazquez <danielvazquezheredia1994@gmail.com>
2022-09-26 19:25:37 +0200

    cambiar presupuesto

--- controller/budget.controller.js
+++ controller/budget.controller.js
@@ -42,4 +42,39 @@ function getBudgetPercent(req, res){
     });
 }
 
-module.exports = {getBudgetPercent}
+function postBudget(req, res){
+    const sql = 
+        `UPDATE hogares SET budget = ${req.body.budget}
+        WHERE (id_hogar = ${req.body.id_hogar})
+        `
+
+        db.query(sql ,(error, result) => {
+
+            if(error){
+                console.error('ERROR UPDATING BUDGET');
+                console.error(error);
+
+                const response = {
+                    error: true,
+                    code: 400,
+                    message: error.message
+                };
+
+                res.send(response);
+
+            }else{
+
+                const response = {
+                    error: false,
+                    code: 200,
+                    data:result
+                };
+
+                res.send(response);
+
+            }
+        
+        });
+}
+
+module.exports = {getBudgetPercent, postBudget}
