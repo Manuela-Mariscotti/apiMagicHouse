09f7dabb822dff2e0470d4b5c2cdaca1d10daa8b
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-09-26 09:40:46 +0200

    show-task controller

--- router/task.router.js
+++ router/task.router.js
@@ -2,7 +2,9 @@ const Router = require ("express");
 const router = Router();
 const taskCtrl = require ("../controller/task.controller");
 
-router.get('/task', taskCtrl.getTasks)
+router.get('/task', taskCtrl.getTasks);
+
+router.get('/taskbyhome',taskCtrl.getTaskByHome);
 
 router.post('/task', taskCtrl.postNewTask);
 
